1542a8bd0008701082b8867c3d0d75587b4fa38c
Alex Mingoia <talk@alexmingoia.com>
2014-06-27 21:47:45 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,29 @@
+# gulp-jsx
+
+[![Build Status](https://secure.travis-ci.org/alexmingoia/gulp-jsx.png)](http://travis-ci.org/alexmingoia/gulp-jsx) 
+[![NPM version](https://badge.fury.io/js/gulp-jsx.png)](http://badge.fury.io/js/gulp-jsx)
+[![Dependency Status](https://david-dm.org/alexmingoia/gulp-jsx.png)](http://david-dm.org/alexmingoia/gulp-jsx)
+
+[virtual-dom-jsx](https://github.com/alexmingoia/virtual-dom-jsx/) for
+[Gulp](https://github.com/gulpjs/gulp/).
+
+## Installation
+
+```sh
+npm install gulp-jsx
+```
+
+## Usage
+
+```javascript
+var gulp = require('gulp');
+var jsx = require('gulp-jsx');
+
+gulp.task('build', function() {
+  return gulp.src('views/**/*.js')
+    .pipe(jsx())
+    .pipe(gulp.dest('dist'));
+});
+```
+
+## BSD Licensed
---
+++ test/jsx.js
@@ -0,0 +1,39 @@
+var expect = require('expect.js');
+var gulp = require('gulp');
+var File = require('vinyl');
+var jsx = require('..');
+
+describe('gulp-jsx', function() {
+  it('returns passthrough stream', function() {
+    var stream = jsx();
+    expect(stream).to.have.property('on');
+    expect(stream).to.have.property('write');
+    expect(stream).to.have.property('end');
+  });
+
+  it('transforms js files in stream', function(done) {
+    var stream = jsx({
+      jsx: "virtualdom.h",
+      ignoreDocblock: true
+    });
+
+    var file = new File({
+      cwd: "",
+      base: "",
+      path: "some.js",
+      contents: new Buffer("<div></div>", 'utf8')
+    });
+
+    stream.on('data', function(chunk) {
+      expect(chunk).to.equal(file);
+    });
+
+    stream.on('end', function() {
+      expect(file.contents.toString()).to.contain("virtualdom.h('div");
+      done();
+    });
+
+    stream.write(file);
+    stream.end();
+  });
+});
